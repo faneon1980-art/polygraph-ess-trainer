f7b967ff63c9e8499572c39420cbcee3e201b92d
faneon1980-art <faneon1980@gmail.com>
2025-12-16 14:22:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import gaussian
+from scipy.signal.windows import gaussian
 import random
 
 # -------------------------------------------------
